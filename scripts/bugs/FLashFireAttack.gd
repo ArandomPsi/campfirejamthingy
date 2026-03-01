0c5f973d794334ae57905819491e949c17c8b01a
Kosm0-o <logantreboutat@gmail.com>
2026-03-01 15:58:44 -0800

    fire attack

---
+++ scripts/bugs/FLashFireAttack.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var i = [-1, 1].pick_random()
+	$FlipAxis.scale.x = i
+	$AnimationPlayer.play("flash")
+	await $AnimationPlayer.animation_finished
+	$FlipAxis/GPUParticles2D.emitting = true
+	await get_tree().create_timer(0.5).timeout
+	$FlipAxis/Hitbox/CollisionShape2D.disabled = false
+	await $FlipAxis/GPUParticles2D.finished
+	queue_free()
+
+
